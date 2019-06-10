c56310fb0fb1be08c008d7c0bd776e3d71d276a5
Berisan <berisan@berisan.dev>
2019-06-11 01:30:39 +0200

    Code cleanup and optimization

--- MonsterMenu.cs
+++ MonsterMenu.cs
@@ -3,7 +3,6 @@ using StardewValley.Menus;
 using StardewValley;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Linq;
 using System;
 
 namespace Spawn_Monsters
@@ -17,15 +16,7 @@ namespace Spawn_Monsters
 		private ClickableTextureComponent rightArrow;
 
 		private List<List<ClickableComponent>> Pages { get; set; } = new List<List<ClickableComponent>>();
-		private List<ClickableComponent> CurrentPage { get; set; } = new List<ClickableComponent>();
-		private int CurrentPageI { get; set; }
-		private int SlimeColor { get; set; }
-		private int BatColor { get; set; }
-		private int GhostColor { get; set; }
-		private int CrabColor { get; set; }
-		private int FlyColor { get; set; }
-		private int GrubColor { get; set; }
-		private int GolemColor { get; set; }
+		private int currentPage = 0;
 
 		public MonsterMenu()
 			: base(0, 0, 0, 0, true) {
@@ -40,19 +31,19 @@ namespace Spawn_Monsters
 			rightArrow = new ClickableTextureComponent(new Rectangle(this.xPositionOnScreen + this.width / 3 * 2, this.yPositionOnScreen + this.height, 44, 48), Game1.mouseCursors, new Rectangle(365, 495, 12, 11), 4f, false);
 
 			List<ClickableComponent> page0 = new List<ClickableComponent> {
-				new MultiClickableMonsterComponent(new string[]{ "Green Slime", "Green Slime", "Green Slime", "Green Slime", "Green Slime", "Green Slime" }, new Color[]{ Color.Green, Color.DarkTurquoise, Color.Red, Color.Purple, Color.Yellow, Color.Black }, xPositionOnScreen, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ new object[] { 0 }, new object[] { 40 }, new object[] { 80 }, new object[] { 121 }, new object[] { new Color(255, 255, 50) }, new object[] { new Color(40 + r.Next(10), 40 + r.Next(10), 40 + r.Next(10)) } }, 16, 24) {
+				new MultiClickableMonsterComponent(new string[]{ "Green Slime", "Green Slime", "Green Slime", "Green Slime", "Green Slime", "Green Slime" }, new Color[]{ Color.Green, Color.DarkTurquoise, Color.Red, Color.Purple, Color.Yellow, Color.Black }, xPositionOnScreen, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ 0, 40, 80, 121, new Color(255, 255, 50), new Color(40 + r.Next(10), 40 + r.Next(10), 40 + r.Next(10)) } , 16, 24) {
 					myID = 0,
 					rightNeighborID = 1,
 					downNeighborID = 2,
 				},
 
-				new MultiClickableMonsterComponent(new string[]{ "Bat", "Frost Bat", "Lava Bat", "Iridium Bat"}, new Color[]{ Color.Brown, Color.Blue, Color.DarkRed, Color.Purple}, xPositionOnScreen + this.width / 2, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ null, new object[] { 40 }, new object[] { 80 }, new object[] { 171 } }) {
+				new MultiClickableMonsterComponent(new string[]{ "Bat", "Frost Bat", "Lava Bat", "Iridium Bat"}, new Color[]{ Color.Brown, Color.Blue, Color.DarkRed, Color.Purple}, xPositionOnScreen + this.width / 2, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ null, 40, 80, 171 }) {
 					myID = 1,
 					leftNeighborID = 0,
 					downNeighborID = 3
 				},
 
-				new MultiClickableMonsterComponent(new string[]{ "Bug", "Armored Bug" }, new Color[]{ Color.DarkTurquoise, Color.Salmon}, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ new object[]{ 0 }, new object[] { 121 } }, 16, 16) {
+				new MultiClickableMonsterComponent(new string[]{ "Bug", "Armored Bug" }, new Color[]{ Color.DarkTurquoise, Color.Salmon}, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ 0, 121 }, 16, 16) {
 					myID = 2,
 					rightNeighborID = 3,
 					upNeighborID = 0
@@ -73,19 +64,19 @@ namespace Spawn_Monsters
 					downNeighborID = 2
 				},
 
-				new MultiClickableMonsterComponent(new string[]{"Fly", "Fly" }, new Color[]{ Color.White, Color.Lime }, xPositionOnScreen + this.width / 2, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ null, new object[] { true } }) {
+				new MultiClickableMonsterComponent(new string[]{"Fly", "Fly" }, new Color[]{ Color.White, Color.Lime }, xPositionOnScreen + this.width / 2, yPositionOnScreen, this.width / 2, this.height / 2, new object[]{ null, true }) {
 					myID = 1,
 					leftNeighborID = 0,
 					downNeighborID = 3
 				},
 
-				new MultiClickableMonsterComponent(new string[]{"Ghost", "Carbon Ghost" }, new Color[]{ Color.Beige, Color.Gray }, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, new object[] { "Carbon Ghost" } }) {
+				new MultiClickableMonsterComponent(new string[]{"Ghost", "Carbon Ghost" }, new Color[]{ Color.Beige, Color.Gray }, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null,  "Carbon Ghost" }) {
 					myID = 2,
 					rightNeighborID = 3,
 					upNeighborID = 0
 				},
 
-				new MultiClickableMonsterComponent(new string[]{"Grub", "Grub" }, new Color[]{ Color.White, Color.Lime }, xPositionOnScreen + this.width / 2, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, new object[] { true } }) {
+				new MultiClickableMonsterComponent(new string[]{"Grub", "Grub" }, new Color[]{ Color.White, Color.Lime }, xPositionOnScreen + this.width / 2, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, true }) {
 					myID = 3,
 					leftNeighborID = 2,
 					upNeighborID = 1
@@ -106,13 +97,13 @@ namespace Spawn_Monsters
 					downNeighborID = 3
 				},
 
-				new MultiClickableMonsterComponent(new string[]{ "Rock Crab", "Lava Crab", "Iridium Crab"}, new Color[]{ Color.Gray, Color.DarkRed, Color.Violet }, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, null, new object[]{ "Iridium Crab"} }) {
+				new MultiClickableMonsterComponent(new string[]{ "Rock Crab", "Lava Crab", "Iridium Crab"}, new Color[]{ Color.Gray, Color.DarkRed, Color.Violet }, xPositionOnScreen, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, null, "Iridium Crab" }) {
 					myID = 2,
 					rightNeighborID = 3,
 					upNeighborID = 0
 				},
 
-				new MultiClickableMonsterComponent(new string[]{"Stone Golem", "Wilderness Golem" }, new Color[]{ Color.Gray, Color.Green }, xPositionOnScreen + this.width / 2, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, new object[] { 5 } }) {
+				new MultiClickableMonsterComponent(new string[]{"Stone Golem", "Wilderness Golem" }, new Color[]{ Color.Gray, Color.Green }, xPositionOnScreen + this.width / 2, yPositionOnScreen + this.height / 2, this.width / 2, this.height / 2, new object[]{ null, 5 }) {
 					myID = 3,
 					leftNeighborID = 2,
 					upNeighborID = 1
@@ -161,9 +152,6 @@ namespace Spawn_Monsters
 			Pages.Add(page3);
 			Pages.Add(page4);
 
-			CurrentPage = page0;
-			CurrentPageI = 0;
-
 			this.initializeUpperRightCloseButton();
 			if (!Game1.options.snappyMenus || !Game1.options.gamepadControls)
 				return;
@@ -180,7 +168,7 @@ namespace Spawn_Monsters
 
 			b.DrawString(Game1.dialogueFont, "Select a\nmonster\nto spawn", new Vector2(xPositionOnScreen - 240, yPositionOnScreen + 110), Color.Black);
 
-			foreach (ClickableComponent c in CurrentPage) {
+			foreach (ClickableComponent c in Pages[currentPage]) {
 				if(c.GetType() == typeof(MultiClickableMonsterComponent)) {
 					MultiClickableMonsterComponent m = (MultiClickableMonsterComponent)c;
 					m.Draw(b);
@@ -194,7 +182,7 @@ namespace Spawn_Monsters
 			leftArrow.draw(b);
 			rightArrow.draw(b);
 
-			b.DrawString(Game1.dialogueFont, $"{CurrentPageI+1}/{Pages.Count}", new Vector2(this.xPositionOnScreen + (float)((this.width / 3) * 1.5), this.yPositionOnScreen + this.height), Color.White);
+			b.DrawString(Game1.dialogueFont, $"{currentPage+1}/{Pages.Count}", new Vector2(this.xPositionOnScreen + (float)((this.width / 3) * 1.5), this.yPositionOnScreen + this.height), Color.White);
 
 			this.drawMouse(b);
 			base.draw(b);
@@ -203,11 +191,11 @@ namespace Spawn_Monsters
 		public override void receiveLeftClick(int x, int y, bool playSound = true) {
 			if (this.isWithinBounds(x, y)) {
 
-				foreach (ClickableComponent monster in CurrentPage) {
+				foreach (ClickableComponent monster in Pages[currentPage]) {
 
 					if(monster.GetType() == typeof(MultiClickableMonsterComponent)) {
 						MultiClickableMonsterComponent m = (MultiClickableMonsterComponent)monster;
-						m.receiveLeftClick(x, y);
+						m.ReceiveLeftClick(x, y);
 					} else if (monster.containsPoint(x, y)) {
 						Game1.activeClickableMenu = new MonsterPlaceMenu(monster.name, null);
 					}
@@ -216,24 +204,20 @@ namespace Spawn_Monsters
 
 			} else if (leftArrow.containsPoint(x, y)) {
 
-				CurrentPageI--;
-				if (CurrentPageI < 0) {
-					CurrentPage = Pages.ElementAt(Pages.Count - 1);
-					CurrentPageI = Pages.Count - 1;
-				} else {
-					CurrentPage = Pages.ElementAt(CurrentPageI);
+				currentPage--;
+				if (currentPage < 0) {
+					currentPage = Pages.Count - 1;
 				}
 				Game1.playSound("smallSelect");
+
 			} else if (rightArrow.containsPoint(x, y)) {
 
-				CurrentPageI++;
-				if (CurrentPageI == Pages.Count) {
-					CurrentPage = Pages.ElementAt(0);
-					CurrentPageI = 0;
-				} else {
-					CurrentPage = Pages.ElementAt(CurrentPageI);
+				currentPage++;
+				if (currentPage == Pages.Count) {
+					currentPage = 0;
 				}
 				Game1.playSound("smallSelect");
+
 			} else {
 				Game1.exitActiveMenu();
 			}
@@ -241,21 +225,21 @@ namespace Spawn_Monsters
 
 		public override void performHoverAction(int x, int y) {
 			base.performHoverAction(x, y);
-			foreach (ClickableComponent monster in this.CurrentPage) {
+			foreach (ClickableComponent monster in Pages[currentPage]) {
 				if (monster.GetType() == typeof (ClickableMonsterComponent)) {
 					ClickableMonsterComponent m = (ClickableMonsterComponent)monster;
 					m.PerformHoverAction(x, y);
 				} else {
 					MultiClickableMonsterComponent m = (MultiClickableMonsterComponent)monster;
-					m.performHoverAction(x, y);
+					m.PerformHoverAction(x, y);
 				}
 					
 			}
 		}
 
 		public override void snapToDefaultClickableComponent() {
-			this.currentlySnappedComponent = this.getComponentWithID(0);
-			this.snapCursorToCurrentSnappedComponent();
+			currentlySnappedComponent = getComponentWithID(0);
+			snapCursorToCurrentSnappedComponent();
 		}
 	}
 }
--- MonsterPlaceMenu.cs
+++ MonsterPlaceMenu.cs
@@ -14,127 +14,111 @@ namespace Spawn_Monsters
 	class MonsterPlaceMenu : IClickableMenu
 	{
 		private ClickableTextureComponent ok;
-
-		public Type Monster { get; set; }
-		public int Area { get; set; }
-		public object[] Args { get; set; }
-		public bool ShouldShow { get; set; } = true;
 		private Texture2D placementTile;
+		public Type Monster { get; set; }
+		public object[] Args { get; set; }
 
-		public MonsterPlaceMenu(string name, object[] args, int arealevel = 0)
+		public MonsterPlaceMenu(string name, object arg)
 			: base(0, 0, Game1.viewport.Width, Game1.viewport.Height) {
 
 			Assembly a = Assembly.GetAssembly(new Monster().GetType());
 
+			//prepare arguments, we will have 2 args if we passed an additional argument
+			if (arg != null) {
+				Args = new object[2];
+				Args[1] = arg;
+			} else Args = new object[1];
+
+			//Determine type of monster to spawn
 			switch (name) {
 				case "Green Slime":
 					Monster = Type.GetType("StardewValley.Monsters.GreenSlime, " + a);
-					Args = new object[2];
-					Args[1] = args[0];
 					break;
-				case "Bat":
-					Monster = Type.GetType("StardewValley.Monsters.Bat, " + a);
-					if (args != null) {
-						Args = new object[2];
-						Args[1] = args[0];
-					} else {
-						Args = new object[1];
-					}
-					
-					break;
-				case "Bug":
-					Monster = Type.GetType("StardewValley.Monsters.Bug, " + a);
-					Args = new object[2];
-					Args[1] = args[0];
 
-					if((int)args[0] == 121) {
-						Game1.addHUDMessage(new HUDMessage("Be aware that armored bugs are unkillable.",2));
-					}
+				case "Bat":
+				case "Frost Bat":
+				case "Lava Bat":
+				case "Iridium Bat":
+					Monster = Type.GetType("StardewValley.Monsters.Bat, " + a);	
 					break;
+
+				case "Bug":
+				case "Armored Bug":
+					Monster = Type.GetType("StardewValley.Monsters.Bug, " + a);
+					if((int)arg == 121) Game1.addHUDMessage(new HUDMessage("Be aware that armored bugs are unkillable.",2));
+					break;
+
 				case "Duggy":
 					Monster = new DuggyFixed(new Vector2(0,0)).GetType();
-					Args = new object[1];
 					Game1.addHUDMessage(new HUDMessage("Duggies can only be spawned on diggable tiles.", 2));
 					break;
+
 				case "Dust Spirit":
 					Monster = Type.GetType("StardewValley.Monsters.DustSpirit, " + a);
-					Args = new object[1];
 					break;
+
 				case "Fly":
 					Monster = Type.GetType("StardewValley.Monsters.Fly, " + a);
-					if(args != null) {
-						Args = new object[2];
-						Args[1] = args[0];
-					} else Args = new object[1];
 					break;
+
 				case "Ghost":
+				case "Carbon Ghost":
 					Monster = Type.GetType("StardewValley.Monsters.Ghost, " + a);
-					if (args != null) {
-						Args = new object[2];
-						Args[1] = args[0];
-					} else Args = new object[1];
 					break;
+
 				case "Grub":
 					Monster = Type.GetType("StardewValley.Monsters.Grub, " + a);
-					if (args != null) {
-						Args = new object[2];
-						Args[1] = args[0];
-					} else Args = new object[1];
 					break;
+
 				case "Lava Crab":
 					Monster = Type.GetType("StardewValley.Monsters.LavaCrab, " + a);
-					Args = new object[1];
 					break;
+
 				case "Metal Head":
 					Monster = Type.GetType("StardewValley.Monsters.MetalHead, " + a);
 					Args = new object[2];
 					break;
+
 				case "Mummy":
 					Monster = Type.GetType("StardewValley.Monsters.Mummy, " + a);
-					Args = new object[1];
 					break;
+
 				case "Rock Crab":
+				case "Iridium Crab":
 					Monster = Type.GetType("StardewValley.Monsters.RockCrab, " + a);
-					if(args != null) {
-						Args = new object[2];
-						Args[1] = "Iridium Crab";
-					} else {
-						Args = new object[1];
-					}
 					break;
+
 				case "Stone Golem":
+				case "Wilderness Golem":
 					Monster = Type.GetType("StardewValley.Monsters.RockGolem, " + a);
-					if(args != null) {
-						Args = new object[2];
-						Args[1] = args[0];
-					} else Args = new object[1];
 					break;
+
 				case "Serpent":
 					Monster = Type.GetType("StardewValley.Monsters.Serpent, " + a);
-					Args = new object[1];
 					break;
+
 				case "Shadow Brute":
 					Monster = Type.GetType("StardewValley.Monsters.ShadowBrute, " + a);
-					Args = new object[1];
 					break;
+
 				case "Shadow Shaman":
 					Monster = Type.GetType("StardewValley.Monsters.ShadowShaman, " + a);
-					Args = new object[1];
 					break;
+
 				case "Skeleton":
 					Monster = Type.GetType("StardewValley.Monsters.Skeleton, " + a);
-					Args = new object[1];
 					break;
+
 				case "Squid Kid":
 					Monster = Type.GetType("StardewValley.Monsters.SquidKid, " + a);
-					Args = new object[1];
 					break;
 			}
+			
+
 			Game1.playSound("bigSelect");
-			Area = arealevel;
 			ok = new ClickableTextureComponent(new Rectangle(16, 16, 60, 60), Game1.mouseCursors, new Rectangle(128, 256, 63, 63), 1f, false);
-			this.placementTile = Game1.content.Load<Texture2D>("LooseSprites\\buildingPlacementTiles");
-			Game1.addHUDMessage(new HUDMessage($"Click anywhere to spawn a {Monster.Name.Replace("Fixed", "").Replace("Green", "")}", null));
+			placementTile = Game1.content.Load<Texture2D>("LooseSprites\\buildingPlacementTiles");
+			Game1.addHUDMessage(new HUDMessage($"Click anywhere to spawn a {name.Replace("Green ", "")}", null));
 		}
 
 		public override void receiveLeftClick(int x, int y, bool playSound = true) {
@@ -152,8 +136,6 @@ namespace Spawn_Monsters
 				m.currentLocation = Game1.currentLocation;
 				m.setTileLocation(new Vector2(Game1.currentCursorTile.X, Game1.currentCursorTile.Y));
 				Game1.currentLocation.addCharacter(m);
-
-				ShouldShow = false;
 				Game1.playSound("axe");
 			}
 			base.receiveLeftClick(x, y, playSound);
